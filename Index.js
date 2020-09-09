7975047bdc301379221c20727949e6958b966f06
Brian Kimbrow <bkimbrow25@gmail.com>
2020-09-09 14:41:32 -0500

    Week2Homework

---
+++ Index.js
@@ -0,0 +1,11 @@
+let PizzaPlace = "Jumbo Slice";
+let numberOfToppings = 5;
+
+console.log(typeof pizzaPlace);
+console.log(typeof numberOfToppings);
+
+if (numberOfToppings < 10) {
+  console.log("Quality, not quanity");
+} else {
+  console.log("A Whole lot of pizza");
+}
